eaf5b3715e4fb27aa7efeb05031391fc903eef6f
Tashfiq Ahmed Emon <emon@MacbookPro.local>
2020-10-09 04:03:11 +0600

    front end completed

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "iPhone 11 Pro 256GB Memory",
+    image: "/images/phone.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Cannon EOS 80D DSLR Camera",
+    image: "/images/camera.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Cannon",
+    category: "Electronics",
+    price: 929.99,
+    countInStock: 5,
+    rating: 4.0,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Sony Playstation 4 Pro White Version",
+    image: "/images/playstation.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "sony",
+    category: "Electronics",
+    price: 399.99,
+    countInStock: 11,
+    rating: 5.0,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Logitech G-Series Gaming Mouse",
+    image: "/images/mouse.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Logitech",
+    category: "electronics",
+    price: 49.99,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "6",
+    name: "Amazon Echo Dot 3rd Generation",
+    image: "/images/alexa.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Logitech",
+    category: "electronics",
+    price: 29.99,
+    countInStock: 0,
+    rating: 4.0,
+    numReviews: 12,
+  },
+];
+
+export default products;
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import products from "../products";
+import { Row, Col } from "react-bootstrap";
+
+import Product from "../components/Product";
+
+const HomeScreen = () => {
+  return (
+    <Fragment>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </Fragment>
+  );
+};
+
+export default HomeScreen;
